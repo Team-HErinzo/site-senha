93f714b94bcce15a7b810c23b96130f56caadc43
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-07-24 09:27:56 -0300

    .

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto')
 const botoes = document.querySelectorAll('.parametro-senha_botao')
+
+console.log(botoes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 numeroSenha.textContent = 6
