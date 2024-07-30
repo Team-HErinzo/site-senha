e50c88907da7c896a57f25e56e0e34872b37b536
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-07-30 07:46:13 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha_texto')
 const botoes = document.querySelectorAll('.parametro-senha_botao')
 
 botoes[0].onclick = diminuirTamanho
+botoes[1].onclick = aumentarTamanho
 
 let tamanhoSenha = 5
 numeroSenha.textContent = tamanhoSenha
@@ -12,3 +13,8 @@ function diminuirTamanho(){
     numeroSenha.textContent = tamanhoSenha
 }
 
+function aumentarTamanho(){
+    console.log('teste')
+    tamanhoSenha = tamanhoSenha + 1
+    numeroSenha.textContent = tamanhoSenha
+}
